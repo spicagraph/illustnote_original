f0de5d9e5321d844c2e0a43b12eac6a8258bec43
ayaka sumida <aya@spicagraph.com>
2015-09-15 03:19:12 +0900

    mod files

--- footer.php
+++ footer.php
@@ -32,18 +32,6 @@
 <?php wp_footer(); ?>
 
 <script type="text/javascript">
-jQuery(function(){
-	jQuery('.singlePost h2').each(function(i){
-		if(i < 9){
-			jQuery(this).attr('class','h20' + (i+1));
-		} else {
-			jQuery(this).attr('class','h2' + (i+1));
-		}
-	});
-});
-</script>
-
-<script type="text/javascript">
 
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-36393548-1']);
