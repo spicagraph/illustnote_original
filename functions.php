7293713b65a88f9848a183493f7aad870cf5d6b7
ayaka sumida <aya@spicagraph.com>
2015-09-25 16:36:20 +0900

    change files

--- functions.php
+++ functions.php
@@ -9,3 +9,24 @@ function category_id_class( $classes ) {
 }
 add_filter( 'body_class', 'category_id_class' );
 add_theme_support('post-thumbnails');
+
+// カテゴリーのスラッグをwp_list_categoriesへ追加
+function add_cat_slug_class( $output, $args ) {
+    $regex = '/<li class="cat-item cat-item-([\d]+)[^"]*">/';
+    $taxonomy = isset( $args['taxonomy'] ) && taxonomy_exists( $args['taxonomy'] ) ? $args['taxonomy'] : 'category';
+     
+    preg_match_all( $regex, $output, $m );
+     
+    if ( ! empty( $m[1] ) ) {
+        $replace = array();
+        foreach ( $m[1] as $term_id ) {
+            $term = get_term( $term_id, $taxonomy );
+            if ( $term && ! is_wp_error( $term ) ) {
+                $replace['/<li class="cat-item cat-item-' . $term_id . '("| )/'] = '<li class="cat-item cat-item-' . $term_id . ' cat-item-' . esc_attr( $term->slug ) . '$1';
+            }
+        }
+        $output = preg_replace( array_keys( $replace ), $replace, $output );
+    }
+    return $output;
+}
+add_filter( 'wp_list_categories', 'add_cat_slug_class', 10, 2 );
